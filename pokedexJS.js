75263eeedc51343bbc223e62bf6892f69b11aae5
Daniel Espinoza <62618610+dalexez@users.noreply.github.com>
2022-03-21 12:26:21 -0600

    Add files via upload

--- pokedexJS.js
+++ pokedexJS.js
@@ -5,7 +5,6 @@ const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/${pkmnToSearch}`;
     fetch(url).then((res) => {
         if (res.status != "200") {
-            console.log(res);
             pokeImage("./pokemon-sad.gif")
         }
         else {
@@ -13,13 +12,10 @@ const fetchPokemon = () => {
         }
     }).then((datos) => {
         if (datos) {
-            console.log(datos);
             let pokeImg = datos.sprites.front_default;
             pokeImage(datos);
-            console.log(pokeImg);
         }
     });
-    
 }
 
 const pokeImage = (datos) => {
@@ -49,30 +45,14 @@ const pokeImage = (datos) => {
     }
     //
 
-    // //Esto es para calcular el ancho de la barra de stats
-    // let progress = document.getElementsByClassName("progress");
-    // let bars = document.getElementsByClassName("bar");
-
-    // console.log(progress)
-
-    // for (let i = 0; i < progress.length; i++) {
-    //     bars[i].style.width = (parseInt(progress[i].innerText)/2) + "%";
-    // }
-    // //
-
-
     typesPoke = datos.types.map(function(tipos){ //Esto saca los tipo de pokemon y los mete en un arreglo todos juntos
         return `<p class="types">${tipos.type.name}</p>`
     })
 
+    //Para conseguir los movimientos iniciales del pokemon
     abilitiesPkmn = datos.abilities.map(function(habilidades){
         return `<span>${habilidades.ability.name}</span>`
     })
-
-    console.log(abilitiesPkmn);
-
-    //pruebaInnerHTML = typesPoke.join(" ")
-    //console.log(pruebaInnerHTML)
 
     document.getElementById("pokeName").innerText = datos.name
     document.getElementById("pokeNumber").innerText = number
@@ -116,47 +96,6 @@ const pokeImage = (datos) => {
         return res.json();
     }).then((datos) => {
         document.getElementById("category").innerText = datos.genera[7].genus;
-        //pokeSpecies(datos);
-        console.log(datos.genera[7].genus)
     });
     // 
-
-    
-
-    // const pokeSpecies = (datos) => {
-    //     document.getElementById("category".innerText = datos.genera[7].genus);
-    //     console.log("HOLA") 
-    // }
-    
 }
-
-
-
-
-
-
-// var numbers = [1, 5, 10, 15];
-// var doubles = numbers.map(function(x) {
-//    return x * 2;
-// });
-
-// // doubles is now [2, 10, 20, 30]
-// // numbers is still [1, 5, 10, 15]
-
-// var numbers = [1, 4, 9];
-// var roots = numbers.map(function(num) {
-//     return Math.sqrt(num);
-// });
-// // roots is now [1, 2, 3]
-// // numbers is still [1, 4, 9]
-
-// const materials = [
-//     'Hydrogen',
-//     'Helium',
-//     'Lithium',
-//     'Beryllium'
-//   ];
-  
-//   console.log(materials.map(material => material.length));
-//   // expected output: Array [8, 6, 7, 9]
-//   console.log(materials)
